f817a5d0f94b273a366488157c6ef3ff1c647baf
taranu <dtaranu@astro.princeton.edu>
2023-07-25 11:08:34 -0700

    Fix parameter initialization bugs

--- python/lsst/meas/extensions/multiprofit/fit_coadd_multiband.py
+++ python/lsst/meas/extensions/multiprofit/fit_coadd_multiband.py
@@ -153,8 +153,10 @@ class MultiProFitSourceTask(CatalogSourceFitterABC, fitMB.CoaddMultibandFitSubTa
         values_init
             Initial values to set per parameter type.
         """
-        params_free = get_params_uniq(component)
-        for param in params_free:
+        # These aren't necessarily all free - should set cen_x, y
+        # even if they're fixed, for example
+        params_init = get_params_uniq(component)
+        for param in params_init:
             type_param = type(param)
             if (value := values_init.get(type_param)) is not None:
                 if param.limits.check(value):
@@ -210,14 +212,7 @@ class MultiProFitSourceTask(CatalogSourceFitterABC, fitMB.CoaddMultibandFitSubTa
         limits_x.max = x_max
         limits_y.max = y_max
         try:
-            cenx_img, ceny_img = self.catexps[0].exposure.wcs.skyToPixel(
-                geom.SpherePoint(source['coord_ra'], source['coord_dec'])
-            )
-            bbox = source.getFootprint().getBBox()
-            beginx, beginy = bbox.beginX, bbox.beginY
-            # multiprofit bottom left corner coords are 0, 0, not -0.5, -0.5
-            cenx = cenx_img - beginx + 0.5
-            ceny = ceny_img - beginy + 0.5
+            cenx, ceny = self.get_model_cens(source)
         # TODO: determine which exceptions can occur above
         except Exception:
             cenx = observation.image.n_cols/2.
@@ -229,20 +224,32 @@ class MultiProFitSourceTask(CatalogSourceFitterABC, fitMB.CoaddMultibandFitSubTa
             g2f.CentroidYParameterD: ceny,
             g2f.ReffXParameterD: sig_x,
             g2f.ReffYParameterD: sig_y,
+            g2f.SigmaXParameterD: sig_x,
+            g2f.SigmaYParameterD: sig_y,
             g2f.RhoParameterD: -rho,
         }
+        # Do not initialize PSF size/rho: they'll all stay zero
+        params_psf_init = (g2f.IntegralParameterD, g2f.CentroidXParameterD, g2f.CentroidYParameterD)
+        values_init_psf = {key: values_init[key] for key in params_psf_init}
         size_major = g2.EllipseMajor(g2.Ellipse(sigma_x=sig_x, sigma_y=sig_y, rho=rho)).r_major
         # An R_eff larger than the box size is problematic
         # Also should stop unreasonable size proposals; log10 transform isn't enough
         # TODO: Try logit for r_eff?
+        flux_max = 5 * max([np.sum(np.abs(datum.image.data)) for datum in model.data])
+        flux_min = 1 / flux_max
+        limits_flux = g2f.LimitsD(flux_min, flux_max, 'unreliable flux limits')
+
         limits_init = {
-            g2f.IntegralParameterD: g2f.LimitsD(1e-6, 1e10),
+            g2f.IntegralParameterD: limits_flux,
             g2f.ReffXParameterD: g2f.LimitsD(1e-5, x_max),
             g2f.ReffYParameterD: g2f.LimitsD(1e-5, y_max),
+            g2f.SigmaXParameterD: g2f.LimitsD(1e-5, x_max),
+            g2f.SigmaYParameterD: g2f.LimitsD(1e-5, y_max),
         }
+        limits_init_psf = {g2f.IntegralParameterD: limits_init[g2f.IntegralParameterD]}
 
         for comp in comps[:n_psfs]:
-            self._init_component(comp, values_init=values_init, limits_init=limits_init)
+            self._init_component(comp, values_init=values_init_psf, limits_init=limits_init_psf)
         for comp in comps[n_psfs:]:
             self._init_component(comp, values_init=values_init, limits_init=limits_init)
         for prior in model.priors:
