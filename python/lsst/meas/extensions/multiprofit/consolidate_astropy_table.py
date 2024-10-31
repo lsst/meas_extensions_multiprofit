4556c6d28a22cd3afb6c4c9694b88eb83d1eb3cb
Dan Taranu <dtaranu@astro.princeton.edu>
2024-10-31 15:10:16 -0700

    Add __all__

--- python/lsst/meas/extensions/multiprofit/consolidate_astropy_table.py
+++ python/lsst/meas/extensions/multiprofit/consolidate_astropy_table.py
@@ -19,6 +19,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+__all__ = (
+    "ConsolidateAstropyTableConfigBase",
+    "ConsolidateAstropyTableConnections",
+    "ConsolidateAstropyTableConfig",
+    "ConsolidateAstropyTableTask",
+)
+
 from collections import defaultdict
 
 import astropy.table as apTab
